f4ffafa696861b87f9f8fb9238eb481bf8ad2f11
Alexey Naumov <alexeynaumov@Lenovo-Legion-5-15ARH05H-267a6435.localdomain>
2023-10-03 12:03:23 +0700

    Format code

--- src/sum/main.cpp
+++ src/sum/main.cpp
@@ -3,12 +3,12 @@
 #include <libsum/libsum.hpp>
 
 int main(int argc, char **argv) {
-  CLI::App app{"sum"};
-  double a = 0;
-  double b = 0;
-  app.add_option("-a", a, "Double");
-  app.add_option("-b", b, "Double");
-  CLI11_PARSE(app, argc, argv);
-  std::cout << sum::sum(a, b) << '\n';
-  return 0;
+    CLI::App app{"sum"};
+    double a = 0;
+    double b = 0;
+    app.add_option("-a", a, "Double");
+    app.add_option("-b", b, "Double");
+    CLI11_PARSE(app, argc, argv);
+    std::cout << sum::sum(a, b) << '\n';
+    return 0;
 }
