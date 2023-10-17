c38e0bac98dbcc0cef7a54d5430104ee5434bb0e
Alexey Naumov <alexeynaumov@Lenovo-Legion-5-15ARH05H-267a6435.localdomain>
2023-10-17 11:53:57 +0700

    Add parser folder with main.cpp and Cmakelist

---
+++ src/parser/main.cpp
@@ -0,0 +1,25 @@
+#include <libparser/libparser.hpp>
+
+#include <iostream>
+#include <string>
+
+int main() {
+    std::cout << "Enter words\n";
+    std::string words;
+    std::getline(std::cin, words);
+
+    VectorOfStrings stop_words;
+    std::string stopword;
+    std::cout << "Enter stop words, push q for end\n";
+    while (std::getline(std::cin, stopword) && stopword != "q") {
+        stop_words.push_back(stopword);
+    }
+
+    int min;
+    int max;
+    std::cout << "Enter min max length\n";
+    std::cin >> min >> max;
+    const VectorOfNgrams result = parser::parse(words, stop_words, min, max);
+    parser::printNgrams(result);
+    return 0;
+}
