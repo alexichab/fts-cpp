502059a5ffae2bfc4051240781a9e927cbadecd0
Alexey Naumov <alexeynaumov@Lenovo-Legion-5-15ARH05H-267a6435.localdomain>
2023-12-13 20:41:28 +0700

    Add libsearcher library

---
+++ src/libsearcher/libsearcher.hpp
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <picosha2.h>
+
+#include "../config/config.hpp"
+#include <libparser.hpp>
+
+#include <algorithm>
+#include <cmath>
+#include <fcntl.h>
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <string>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <unordered_set>
+#include <vector>
+
+struct Result {
+    std::size_t doc_id;
+    double score;
+};
+using Result = struct Result;
+using Results = std::vector<Result>;
+using TermInfos = std::string;
+using SearcherQuery = std::string;
+using DocsCount = std::size_t;
+
+class IndexAccessor {
+  public:
+    virtual Config config() const = 0;
+    virtual TermInfos get_term_infos(const std::string &term) const = 0;
+    virtual std::string load_document(std::size_t doc_id) const = 0;
+    virtual DocsCount total_docs() const = 0;
+};
+
+class TextIndexAccessor : public IndexAccessor {
+  public:
+    explicit TextIndexAccessor(std::filesystem::path &path,
+                               Config &config = DEFAULT_CONFIG)
+        : config_(std::move(config)), path_(std::move(path)){};
+    Config config() const override { return config_; };
+    TermInfos get_term_infos(const std::string &term) const override;
+    std::string load_document(std::size_t doc_id) const override;
+    DocsCount total_docs() const override;
+
+  private:
+    Config config_;
+    std::filesystem::path path_;
+};
+
+namespace searcher {
+
+Results search(const SearcherQuery &query, const IndexAccessor &ia);
+
+} // namespace searcher
