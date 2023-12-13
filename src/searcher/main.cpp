6ed8825de883dad4127d0b7fc04e636da805df57
Alexey Naumov <alexeynaumov@Lenovo-Legion-5-15ARH05H-267a6435.localdomain>
2023-12-13 20:45:53 +0700

    Add main file for libsearcher

---
+++ src/searcher/main.cpp
@@ -0,0 +1,20 @@
+#include <filesystem>
+#include <iostream>
+#include <libsearcher/libsearcher.hpp>
+#include <string>
+
+int main() {
+    std::filesystem::path indexPath = "path/to/output";
+    const TextIndexAccessor indexAccessor(indexPath);
+    const std::string query = "sample";
+    std::cout << "Total docs: " << indexAccessor.total_docs() << '\n';
+    std::cout << "ID\tDocument\t\t\t\tScore\n";
+    const Results results = searcher::search(query, indexAccessor);
+    for (const auto &result : results) {
+        const auto id = result.doc_id;
+        std::cout << id << '\t' << indexAccessor.load_document(id) << '\t'
+                  << result.score << '\n';
+    }
+
+    return 0;
+}
